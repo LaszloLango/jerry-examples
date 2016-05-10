ffacf04f76cea1d0aca7871eb8d93855a65b97a1
László Langó <llango.u-szeged@partner.samsung.com>
2016-05-10 15:38:15 +0200

    Improve the build
    Build JerryScript libs from source
    Remove prebuilt binaries
    
    JerryScript-DCO-1.0-Signed-off-by: László Langó llango.u-szeged@partner.samsung.com

--- build.sh
+++ build.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+CURRENT_DIR=`pwd -P`
+JERRY_HOME="$CURRENT_DIR/deps/source/jerryscript"
+
 rm -rf bin
 mkdir bin
+
+cd $JERRY_HOME
+make clean
+make release.linux
+STATUS=$?
+cd $CURRENT_DIR
+
+if [ $STATUS -ne 0 ]
+then
+  echo "JerryScript build failed"
+  exit $STATUS
+fi
+
+rm -f "$CURRENT_DIR/deps/lib/*.a"
+
+cp "$JERRY_HOME/build/obj/linux/jerry-core/librelease.jerry-core.a" "$CURRENT_DIR/deps/lib/libjerry-core.a"
+cp "$JERRY_HOME/build/obj/linux/jerry-libc/librelease.jerry-libc.linux.lib.a" "$CURRENT_DIR/deps/lib/libjerry-libc.a"
+cp "$JERRY_HOME/build/obj/linux/jerry-libm/librelease.jerry-libm.lib.a" "$CURRENT_DIR/deps/lib/libjerry-libm.a"
 
 gcc -std=c99 -static -g -Wall \
 	-o bin/example1 src/example1.c \
