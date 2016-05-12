18d76995f2ebc3c66d6400d7c9cbec39e2e8fe12
László Langó <llango.u-szeged@partner.samsung.com>
2016-05-12 09:44:49 +0200

    Update build script
     * Remove includes from repository and copy it from JerryScript
     * Init submodule if JerryScript dir is empty
    
    JerryScript-DCO-1.0-Signed-off-by: László Langó llango.u-szeged@partner.samsung.com

--- build.sh
+++ build.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
-CURRENT_DIR=`pwd -P`
-JERRY_HOME="$CURRENT_DIR/deps/source/jerryscript"
+BASE_DIR=`git rev-parse --show-toplevel`
+JERRY_HOME="$BASE_DIR/deps/source/jerryscript"
 CONFIG=debug
-BIN_DIR="$CURRENT_DIR/bin"
+BIN_DIR="$BASE_DIR/bin"
+LIBRARIES_DIR="$BASE_DIR/deps/lib"
+INCLUDE_DIR="$BASE_DIR/deps/include"
+
+IS_JERRY=`ls deps/source/jerryscript`
+
+if [ -z "$IS_JERRY" ]
+then
+  git submodule update --recursive --init
+fi
 
 rm -rf ${BIN_DIR}
 mkdir -p ${BIN_DIR}
 
-rm -rf deps/lib
-mkdir -p deps/lib
+rm -rf ${LIBRARIES_DIR}
+mkdir -p ${LIBRARIES_DIR}
+
+rm -rf ${INCLUDE_DIR}
+mkdir -p ${INCLUDE_DIR}
+
+cp "$JERRY_HOME/jerry-core/jerry-api.h" "$INCLUDE_DIR/jerry-api.h"
+cp "$JERRY_HOME/jerry-core/jerry-port.h" "$INCLUDE_DIR/jerry-port.h"
+cp "$JERRY_HOME/jerry-core/jerry-snapshot.h" "$INCLUDE_DIR/jerry-snapshot.h"
+cp "$JERRY_HOME/jerry-core/jerry.h" "$INCLUDE_DIR/jerry.h"
 
 cd $JERRY_HOME
 make clean
 make ${CONFIG}.linux
 STATUS=$?
-cd $CURRENT_DIR
+cd $BASE_DIR
 
 if [ $STATUS -ne 0 ]
 then
@@ -23,11 +40,9 @@ then
   exit $STATUS
 fi
 
-rm -f "$CURRENT_DIR/deps/lib/*.a"
-
-cp "$JERRY_HOME/build/obj/linux/jerry-core/lib$CONFIG.jerry-core.a" "$CURRENT_DIR/deps/lib/libjerry-core.a"
-cp "$JERRY_HOME/build/obj/linux/jerry-libc/lib$CONFIG.jerry-libc.linux.lib.a" "$CURRENT_DIR/deps/lib/libjerry-libc.a"
-cp "$JERRY_HOME/build/obj/linux/jerry-libm/lib$CONFIG.jerry-libm.lib.a" "$CURRENT_DIR/deps/lib/libjerry-libm.a"
+cp "$JERRY_HOME/build/obj/linux/jerry-core/lib$CONFIG.jerry-core.a" "$LIBRARIES_DIR/libjerry-core.a"
+cp "$JERRY_HOME/build/obj/linux/jerry-libc/lib$CONFIG.jerry-libc.linux.lib.a" "$LIBRARIES_DIR/libjerry-libc.a"
+cp "$JERRY_HOME/build/obj/linux/jerry-libm/lib$CONFIG.jerry-libm.lib.a" "$LIBRARIES_DIR/libjerry-libm.a"
 
 gcc -std=c99 -static -g -Wall \
     -o ${BIN_DIR}/example1 src/example1.c \
