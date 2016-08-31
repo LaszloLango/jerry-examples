b32ce379b22b0457cd5eed38093adb995c1c8273
László Langó <llango.u-szeged@partner.samsung.com>
2016-08-31 11:08:20 +0200

    Update to the new build system
    JerryScript-DCO-1.0-Signed-off-by: László Langó llango.u-szeged@partner.samsung.com

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ BASE_DIR=`git rev-parse --show-toplevel`
 JERRY_HOME="$BASE_DIR/deps/source/jerryscript"
 CONFIG=debug
 BIN_DIR="$BASE_DIR/bin"
-LIBRARIES_DIR="$BASE_DIR/deps/lib"
+LIBRARIES_DIR="$JERRY_HOME/build/lib"
 INCLUDE_DIR="$BASE_DIR/deps/include"
 
 IS_JERRY=`ls deps/source/jerryscript`
@@ -18,9 +18,6 @@ fi
 rm -vrf ${BIN_DIR}
 mkdir -vp ${BIN_DIR}
 
-rm -vrf ${LIBRARIES_DIR}
-mkdir -vp ${LIBRARIES_DIR}
-
 rm -vrf ${INCLUDE_DIR}
 mkdir -vp ${INCLUDE_DIR}
 
@@ -29,9 +26,8 @@ cp -v "$JERRY_HOME/jerry-core/jerry-port.h" "$INCLUDE_DIR/jerry-port.h"
 cp -v "$JERRY_HOME/jerry-core/jerry-snapshot.h" "$INCLUDE_DIR/jerry-snapshot.h"
 
 cd $JERRY_HOME
-VERBOSE=1 make clean
 echo "Building JerryScript..."
-make ${CONFIG}.linux
+./tools/build.py -v --clean --lto=off
 STATUS=$?
 cd $BASE_DIR
 
@@ -41,48 +37,45 @@ then
   exit $STATUS
 fi
 
-cp -v "$JERRY_HOME/build/obj/linux/jerry-core/lib$CONFIG.jerry-core.a" "$LIBRARIES_DIR/libjerry-core.a"
-cp -v "$JERRY_HOME/build/obj/linux/jerry-libc/lib$CONFIG.jerry-libc.linux.lib.a" "$LIBRARIES_DIR/libjerry-libc.a"
-cp -v "$JERRY_HOME/build/obj/linux/jerry-libm/lib$CONFIG.jerry-libm.lib.a" "$LIBRARIES_DIR/libjerry-libm.a"
-
 gcc -std=c99 -nostdlib -static -g -Wall \
     -o ${BIN_DIR}/example1 src/example1.c \
     -I'deps/include' -I'include' \
-    -flto -L'deps/lib' \
+    -isystem "$JERRY_HOME/jerry-libc/include" \
+    -flto -L"$LIBRARIES_DIR" \
     -ljerry-core -ljerry-libm -ljerry-libc -lgcc
 
 gcc -std=c99 -nostdlib -static -g -Wall \
     -o ${BIN_DIR}/example2 src/example2.c \
     -I'deps/include' -I'include' \
-    -flto -L'deps/lib' \
+    -flto -L"$LIBRARIES_DIR" \
     -ljerry-core -ljerry-libm -ljerry-libc -lgcc
 
 gcc -std=c99 -nostdlib -static -g -Wall \
     -o ${BIN_DIR}/example3 src/example3.c \
     -I'deps/include' -I'include' \
-    -flto -L'deps/lib' \
+    -flto -L"$LIBRARIES_DIR" \
     -ljerry-core -ljerry-libm -ljerry-libc -lgcc
 
 gcc -std=c99 -nostdlib -static -g -Wall \
     -o ${BIN_DIR}/example4 src/example4.c \
     -I'deps/include' -I'include' \
-    -flto -L'deps/lib' \
+    -flto -L"$LIBRARIES_DIR" \
     -ljerry-core -ljerry-libm -ljerry-libc -lgcc
 
 gcc -std=c99 -nostdlib -static -g -Wall \
     -o ${BIN_DIR}/example5 src/example5.c \
     -I'deps/include' -I'include' \
-    -flto -L'deps/lib' \
+    -flto -L"$LIBRARIES_DIR" \
     -ljerry-core -ljerry-libm -ljerry-libc -lgcc
 
 gcc -std=c99 -nostdlib -static -g -Wall \
     -o ${BIN_DIR}/example6 src/example6.c \
     -I'deps/include' -I'include' \
-    -flto -L'deps/lib' \
+    -flto -L"$LIBRARIES_DIR" \
     -ljerry-core -ljerry-libm -ljerry-libc -lgcc
 
 gcc -std=c99 -nostdlib -static -g -Wall \
     -o ${BIN_DIR}/example7 src/example7.c \
     -I'deps/include' -I'include' \
-    -flto -L'deps/lib' \
+    -flto -L"$LIBRARIES_DIR" \
     -ljerry-core -ljerry-libm -ljerry-libc -lgcc
